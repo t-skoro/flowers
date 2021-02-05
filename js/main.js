0a4c4fee72309db5600fd676a0da4dee9776fc09
t-skoro <65133778+t-skoro@users.noreply.github.com>
2021-02-06 00:10:23 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,170 @@
+$(document).ready(function () {
+  const flowersSlider = new Swiper('.flowers-slider', {
+    // Optional parameters
+    // Параметры слайдера
+    loop: true,
+    slidesPerView: 6,
+
+    // If we need pagination
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //   },
+
+    // Navigation arrows
+  //   Подключаем стрелки
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+  //   scrollbar: {
+  //     el: '.swiper-scrollbar',
+  //   },
+  // Responsive breakpoints
+  // настройки адаптивной версии
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 2,
+        // spaceBetween: 20
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 3,
+        // spaceBetween: 30
+      },
+      // when window width is >= 992px
+      992: {
+        slidesPerView: 6,
+        // spaceBetween: 40
+      }
+    }
+  });
+
+
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    // Параметры слайдера
+    loop: true,
+    slidesPerView: 1,
+
+    // If we need pagination
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //   },
+
+    // Navigation arrows
+  //   Подключаем стрелки
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+  //   scrollbar: {
+  //     el: '.swiper-scrollbar',
+  //   },
+  });
+
+
+  $("#review-1").on('click', function() {
+
+  $.fancybox.open([
+    {
+      src  : './img/reviews/review-1.jpg',
+      opts : {
+        caption : 'First caption',
+        thumb   : './img/reviews/review-1.jpg'
+      }
+    },
+    {
+      src  : './img/reviews/review-2.jpg',
+      opts : {
+        caption : 'Second caption',
+        thumb   : './img/reviews/review-2.jpg'
+      }
+    },
+    {
+      src  : './img/reviews/review-3.jpg',
+      opts : {
+        caption : 'Second caption',
+        thumb   : './img/reviews/review-3.jpg'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+});
+
+
+$("#review-2").on('click', function() {
+
+  $.fancybox.open([
+    {
+      src  : './img/reviews/review-2.jpg',
+      opts : {
+        caption : 'First caption',
+        thumb   : './img/reviews/review-2.jpg'
+      }
+    },
+    {
+      src  : './img/reviews/review-3.jpg',
+      opts : {
+        caption : 'Second caption',
+        thumb   : './img/reviews/review-3.jpg'
+      }
+    },
+    {
+      src  : './img/reviews/review-1.jpg',
+      opts : {
+        caption : 'Second caption',
+        thumb   : './img/reviews/review-1.jpg'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+});
+$("#review-3").on('click', function() {
+
+  $.fancybox.open([
+    {
+      src  : './img/reviews/review-3.jpg',
+      opts : {
+        caption : 'First caption',
+        thumb   : './img/reviews/review-3.jpg'
+      }
+    },
+    {
+      src  : './img/reviews/review-1.jpg',
+      opts : {
+        caption : 'Second caption',
+        thumb   : './img/reviews/review-1.jpg'
+      }
+    },
+    {
+      src  : './img/reviews/review-2.jpg',
+      opts : {
+        caption : 'Second caption',
+        thumb   : './img/reviews/review-2.jpg'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+});
+
+});
